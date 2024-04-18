b6cd88d53b8a9f10dfc6136b275e77b4e93bf05f
eamonn98 <eamonn98@gmail.com>
2024-04-18 19:36:54 +0100

    Add zero trust research doc and links on main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main>    
+    <a href="SYSARCH2_cloud presentation.pptx">Cloud computing presentation</a> <br>
+    <a href="SYSARCH2 Identity Management Solutions.docx">Identity management</a> <br>
+    <a href="SYSARCH2 cloud application comparison.docx">IaM comparison</a> <br>
+    <a href="SYSARCH2 Zero Trust Research.docx">Zero Trust in Cloud Computing</a> <br>
+    <a href="SYSARCH2 deploying to azure.docx">Azure deployment log</a> <br>
   </main>
 </template>
